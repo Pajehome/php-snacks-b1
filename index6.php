34e3df40f352e69379ab1641800cbea428a8f3bd
Pajehome <abdel20069@gmail.com>
2022-05-23 19:43:28 +0200

    md

---
+++ index6.php
@@ -0,0 +1,88 @@
+<!-- Utilizzare questo array: https://pastebin.com/CkX3680A. Stampiamo il nostro array mettendo gli insegnanti in un rettangolo grigio e i PM in un rettangolo verde. -->
+
+<?php
+
+    $db = [
+        'teachers' => [
+            [
+                'name' => 'Michele',
+                'lastname' => 'Papagni'
+            ],
+            [
+                'name' => 'Fabio',
+                'lastname' => 'Forghieri'
+            ]
+        ],
+        'pm' => [
+            [
+                'name' => 'Roberto',
+                'lastname' => 'Marazzini'
+            ],
+            [
+                'name' => 'Federico',
+                'lastname' => 'Pellegrini'
+            ]
+        ]
+    ];
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <style>
+
+
+
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+        .pm, .teachers {
+            padding: 40px;
+            float: left;
+            width: 50%;
+        }
+        .pm {
+            background: grey;
+        }
+        .teachers {
+            background: green;
+        }
+
+    </style>
+
+</head>
+
+
+<body>
+    
+<div>
+
+<?php 
+        foreach ($db as $key => $value){
+            //var_dump($key);
+            ?> <div class="<?php echo $key ?>">
+            <?php foreach ($value as $value){
+                echo "<p>$value[name] $value[lastname]";
+            } ?>
+            </div> <?php
+        }
+        ?>
+
+  
+</div>
+
+
+</body>
+
+</html>
